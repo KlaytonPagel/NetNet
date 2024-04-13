b8a8c678fe8152bc3e9e90e20c653c6bf4185f49
Klayton Pagel <pagelk@lopers.unk.edu>
2024-04-13 00:28:32 -0500

    Separate Functions

--- code/main.py
+++ code/main.py
@@ -1,10 +1,13 @@
 from scapy.all import *
+from tkinter import *
 
 
 if __name__ == "__main__":
-    # sniff(prn=lambda x:x.show())
 
-    def traffic(pkt):
+    def all_info(pkt):
+        return pkt.show()
+
+    def filtered(pkt):
         return pkt.sprintf("{IP: IPV4 %IP.src% -> %IP.dst%\n}{TCP: Port: %TCP.sport% -> %TCP.dport%\n}")
 
-    sniff(prn=traffic)
+    sniff(prn=all_info)
