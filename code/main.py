19a19b5814d4a4c804c5a2b81001ff9c317dd0fc
Klayton Pagel <pagelk@lopers.unk.edu>
2024-04-13 00:24:47 -0500

    Sniffing

--- code/main.py
+++ code/main.py
@@ -2,4 +2,9 @@ from scapy.all import *
 
 
 if __name__ == "__main__":
-    sniff(prn=lambda x:x.show())
+    # sniff(prn=lambda x:x.show())
+
+    def traffic(pkt):
+        return pkt.sprintf("{IP: IPV4 %IP.src% -> %IP.dst%\n}{TCP: Port: %TCP.sport% -> %TCP.dport%\n}")
+
+    sniff(prn=traffic)
